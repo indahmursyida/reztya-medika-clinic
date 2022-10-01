1890688064f87c47df47d0e3ee13e811a099f45e
indahmursyida <indahbahrina@gmail.com>
2022-10-01 19:31:57 +0700

    Add model and table of service

---
+++ reztya-medika-clinic-web-app/app/Models/Service.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id', 
+        'schedule_id', 
+        'name', 
+        'description', 
+        'price', 
+        'image_path'
+    ];
+    public function category(){
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+    public function schedule(){
+        return $this->hasMany(Schedule::class, 'schedule_id');
+    }
+}
---
+++ reztya-medika-clinic-web-app/database/migrations/2022_09_28_134945_create_services_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id('service_id');
+            $table->foreignId('category_id');
+            $table->foreignId('schedule_id');
+            $table->string('name', 255);
+            $table->text('description');
+            $table->integer('price', 11);
+            $table->string('image_path', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+};
