40360fce00b425e3216f222fd5f082ceadd1fa91
indahmursyida <indahbahrina@gmail.com>
2022-10-14 20:02:25 +0700

    Add manage_product.blade.php

--- reztya-medika-clinic-web-app/database/seeders/DatabaseSeeder.php
+++ reztya-medika-clinic-web-app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +24,22 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Category::create([
+            'category_name' => 'Body Care'
+        ]);
+
+        Category::create([
+            'category_name' => 'SkinCare'
+        ]);
+        
+        Product::create([
+            'name' => 'Body Shower',
+            'category_id' => '1',
+            'description' => 'Untuk mandi',
+            'price' => '150000',
+            'expired_date' => Carbon::create('2024', '08', '23'),
+            'stock' => '20',
+            'image_path' => 'blabla'
+        ]);
     }
 }
