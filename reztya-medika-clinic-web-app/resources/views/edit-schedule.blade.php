72e3cd96698ed377118067d649fca82d4ff284ab
yska02 <yesikaa02@gmail.com>
2022-10-14 17:11:22 +0700

    ubah edit schedule sama manage schedule dikit

--- reztya-medika-clinic-web-app/resources/views/edit-schedule.blade.php
+++ reztya-medika-clinic-web-app/resources/views/edit-schedule.blade.php
@@ -3,20 +3,22 @@
 @section('title', 'Edit Schedule')
 
 @section('container')
-<h1 class="text-center" style="color: #00A54F">Ubah Jadwal</h1>
-<div>
-    <p class="">T</p>
-    <input class="input-data ps-3 pb-1" type="date">
-</div>
-<div>
-    <p class="">Start Time</p>
-    <input class="input-data ps-3 pb-1 form-control" type="time">
-</div>
-<div>
-    <p class="">End Time</p>
-    <input class="input-data ps-3 pb-1 form-control" type="time">
-</div>
-<div>
-    <input type="button" class="btn btn-outline-success" value="Add">
-</div>
+<form action="" method="POST">
+    <h1 class="text-center mb-3">Ubah Jadwal</h1>
+    <div class="mb-3">
+        <p class="mb-2">Tanggal</p>
+        <input class="input-data ps-3 pb-1 form-control" type="date">
+    </div>
+    <div class="mb-3">
+        <p class="mb-2">Waktu Mulai</p>
+        <input class="input-data ps-3 pb-1 form-control" type="time">
+    </div>
+    <div class="mb-3">
+        <p class="mb-2">Waktu Berakhir</p>
+        <input class="input-data ps-3 pb-1 form-control" type="time">
+    </div>
+    <div class="d-flex justify-content-center mt-5">
+        <input type="submit" class="btn btn-outline-success" value="Ubah Jadwal">
+    </div>
+</form>
 @endsection
--- reztya-medika-clinic-web-app/resources/views/manage-schedule.blade.php
+++ reztya-medika-clinic-web-app/resources/views/manage-schedule.blade.php
@@ -6,14 +6,14 @@
 <div class="font-futura-reztya">
     <h1 class="text-center">Jadwal Perawatan</h1>
     <div class="button_new_schedule">
-        <a class="btn btn-outline-success">Tambah Jadwal</a>
+        <a href="{{ url('/add-schedule') }}" class="btn btn-outline-success">Tambah Jadwal</a>
     </div>
-    <div class="schedule pt-5">
-        <div class="d-flex jus align-items-center">
-            <p class="d-flex align-items-center pe-3">Tanggal</p>
+    <div class="schedule pt-5 row">
+        <div class="d-flex align-items-center">
+            <p class="d-flex align-items-center mb-0">Tanggal</p>
         </div>
         <div class="d-flex justify-content-between">
-            <p class="justify-content-star">Waktu Mulai - Waktu Berakhir</p>
+            <p class="d-flex align-self-center mb-0">Waktu Mulai - Waktu Berakhir</p>
             <div class="d-flex">
                 <a href="" type="button" class="btn button-outline-reztya">Ubah</a>
                 <a href="" type="button" class="btn btn-outline-danger">Hapus</a>
