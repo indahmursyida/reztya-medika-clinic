0f6e72b1ae6dc2b8a15b852b62eced05a92c5c7a
Xenon468 <59041286+Xenon468@users.noreply.github.com>
2022-10-16 20:10:07 +0700

    Make Controller singular and routes

---
+++ reztya-medika-clinic-web-app/app/Http/Controllers/SignUpController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class SignUpController extends Controller
+{
+    public function userRegister(Request $request) {
+        $validated = $request->validate([
+            'username' => 'required|max:255',
+            'name' => 'required',
+            'birthdate' => 'required|date',
+            'email' => 'required|unique:users|email:rfc,dns',
+            'password' => 'required',
+            'phone' => 'required',
+            'address' => 'required'
+        ]);
+
+        $confirm_password = $request->validate([
+            'confirm_password' => 'required'
+        ]);
+
+        if ($validated['password'] == $confirm_password['confirm_password']) {
+            $validated['password'] = Hash::make($validated['password']);
+            $validated['phone'] = strval($validated['phone']);
+
+            DB::table('users')->insert($validated);
+            return redirect()->intended('/home')->with('addSuccess', 'Sign up success! Please sign in');
+        }
+
+        return redirect()->back()->with('signupError', 'Sign up error!');
+    }
+}
