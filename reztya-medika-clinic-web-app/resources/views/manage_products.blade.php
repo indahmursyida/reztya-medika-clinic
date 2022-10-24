db7d4d5e2decb925b09776876944b570cf41959f
indahmursyida <indahbahrina@gmail.com>
2022-10-24 21:37:17 +0700

    Adjustment button

--- reztya-medika-clinic-web-app/resources/views/manage_products.blade.php
+++ reztya-medika-clinic-web-app/resources/views/manage_products.blade.php
@@ -7,8 +7,8 @@
     <div class="py-3 text-center">
         <h2 class="pb-5 font-alander-reztya unselectable">Daftar Produk</h2>
     </div>
-    <a href="/add-product" class="btn button-color my-3"><i class="fa-solid fa-plus"></i></a>
-    <table class="table table-borderedtable-striped outline-reztya">
+    <a href="/add-product" class="btn button-outline-reztya my-3"><i class="fa-solid fa-plus"></i> Tambah Product</a>
+    <table class="table table-bordered table-striped outline-reztya">
         <thead>
             <tr class="text-center">
                 <th>No.</th>
@@ -36,11 +36,11 @@
                 <td>{{ $product->stock }} buah</td>
                 <td>Rp. {{  number_format($product->price, 0, '', '.') }}</td>
                 <td>
-                    <a class="btn btn-secondary btn-sm" href="/product-detail/{{ $product->product_id }}"><i class="fa-solid fa-circle-info"></i></a>
-                    <a class="btn btn-primary btn-sm" href="/edit-product/{{ $product->product_id }}"><i class="fa-regular fa-pen-to-square"></i></a>
+                    <a class="btn btn-outline-secondary btn-sm" href="/product-detail/{{ $product->product_id }}"><i class="fa-solid fa-circle-info"></i> Detail</a>
+                    <a class="btn btn-outline-primary btn-sm" href="/edit-product/{{ $product->product_id }}"><i class="fa-regular fa-pen-to-square"></i></a>
                     <form method="post" action="/delete-product/{{ $product->product_id }}" class="d-inline">
                         @method('post') @csrf
-                        <button class="btn btn-danger btn-sm" onclick="return confirm('Apakah anda yakin ingin mengapus produk ini?')">
+                        <button class="btn btn-outline-danger btn-sm" onclick="return confirm('Apakah anda yakin ingin mengapus produk ini?')">
                             <i class="fa-solid fa-trash"></i>
                         </button>
                     </form>
