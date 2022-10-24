529d7f219c0ab73845f6075869f9648fea9e4e24
Xenon468 <59041286+Xenon468@users.noreply.github.com>
2022-10-24 10:50:32 +0700

    Added Admin seeder

--- reztya-medika-clinic-web-app/database/seeders/DatabaseSeeder.php
+++ reztya-medika-clinic-web-app/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,25 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        DB::table('user_role')->insert([
+            'user_role_name' => 'Admin'
+        ]);
+
+        DB::table('user_role')->insert([
+            'user_role_name' => 'Guest'
+        ]);
+
+        DB::table('users')->insert([
+            'user_role_id' => 1,
+            'username' => 'Admin',
+            'name' => 'Admin Admin Admin',
+            'birthdate' => '2001-06-18',
+            'phone' => '081285879816',
+            'address' => 'Your Heart my Darling',
+            'email' => 'harishsaid37@gmail.com',
+            'password' => bcrypt('admin'),
+            'is_banned' => false
+        ]);
     }
 }
