a733496481b22e2fc6bc24450c6e3e046c95c47b
Xenon468 <59041286+Xenon468@users.noreply.github.com>
2022-10-24 22:13:33 +0700

    Changes to error message

--- reztya-medika-clinic-web-app/resources/views/users/signup.blade.php
+++ reztya-medika-clinic-web-app/resources/views/users/signup.blade.php
@@ -2,16 +2,16 @@
 @section('title', 'Daftar')
 
 @section('container')
-    <div class="container signup-box">
+    <div class="container signup-box mb-5">
         @if($errors->any())
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
                 Pendaftaran tidak berhasil!
                 <button type="button" class="btn btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
-        <div class="box-up">
+        <div class="box-up pt-4">
             <div class="d-flex justify-content-center">
-                <p class="h5 fw-bold unselectable" style="font-family: Alander, sans-serif;">Daftar Akun</p>
+                <p class="h5 fw-bold unselectable font-alander-reztya">Daftar Akun</p>
             </div>
         </div>
         <div class="unselectable d-flex justify-content-center mt-2">
@@ -20,74 +20,74 @@
                     <form action="/signup" method="POST">
                         @csrf
                         <div class="form-floating mb-2" autofocus>
-                            <input placeholder="Username" id="floatingUsername" class="shadow-none form-control @error('username') is-invalid @enderror" type="text" name="username" required value="{{old('username')}}" autofocus>
+                            <input placeholder="Username" id="floatingUsername" class="shadow-none form-control @error('username') is-invalid @enderror" type="text" name="username" value="{{old('username')}}" autofocus>
                             <label for="floatingUsername" class="font-futura-reztya">Username</label>
                             @error('username')
                             <div class="invalid-feedback">
-                                Username harap tidak kosong.
+                                Username wajib diisi
                             </div>
                             @enderror
                         </div>
                         <div class="form-floating mb-2">
-                            <input placeholder="Name" id="floatingName" class="shadow-none form-control @error('name') is-invalid @enderror" type="text" name="name" required value="{{old('name')}}">
+                            <input placeholder="Name" id="floatingName" class="shadow-none form-control @error('name') is-invalid @enderror" type="text" name="name" value="{{old('name')}}">
                             <label for="floatingName" class="font-futura-reztya">Nama Lengkap</label>
                             @error('name')
                             <div class="invalid-feedback">
-                                Nama lengkap harap tidak kosong.
+                                Nama lengkap wajib diisi
                             </div>
                             @enderror
                         </div>
                         <div class="form-floating mb-2">
-                            <input placeholder="Birthdate" id="floatingBirthdate" class="shadow-none form-control @error('birthdate') is-invalid @enderror" type="date" name="birthdate" required value="{{old('birthdate')}}">
+                            <input placeholder="Birthdate" id="floatingBirthdate" class="shadow-none form-control @error('birthdate') is-invalid @enderror" type="date" name="birthdate" value="{{old('birthdate')}}">
                             <label for="floatingBirthdate" class="font-futura-reztya">Tanggal Lahir</label>
                             @error('birthdate')
                             <div class="invalid-feedback">
-                                Tanggal lahir harap tidak kosong.
+                                Tanggal lahir wajib diisi
                             </div>
                             @enderror
                         </div>
                         <div class="form-floating mb-2">
-                            <input placeholder="Phone" id="floatingPhone" class="shadow-none form-control @error('phone') is-invalid @enderror" type="number" name="phone" required value="{{old('phone')}}">
+                            <input placeholder="Phone" id="floatingPhone" class="shadow-none form-control @error('phone') is-invalid @enderror" type="number" name="phone" value="{{old('phone')}}">
                             <label for="floatingPhone" class="font-futura-reztya">Nomor Telepon</label>
                             @error('phone')
                             <div class="invalid-feedback">
-                                Nomor telepon harap tidak kosong.
+                                Nomor telepon wajib diisi
                             </div>
                             @enderror
                         </div>
                         <div class="form-floating mb-2">
-                            <input placeholder="Address" id="floatingAddress" class="shadow-none form-control @error('address') is-invalid @enderror" type="text" name="address" required value="{{old('address')}}">
+                            <input placeholder="Address" id="floatingAddress" class="shadow-none form-control @error('address') is-invalid @enderror" type="text" name="address" value="{{old('address')}}">
                             <label for="floatingAddress" class="font-futura-reztya">Alamat</label>
                             @error('address')
                             <div class="invalid-feedback">
-                                Alamat harap tidak kosong.
+                                Alamat wajib diisi
                             </div>
                             @enderror
                         </div>
                         <div class="form-floating mb-2">
-                            <input placeholder="Email" id="floatingEmail" class="shadow-none form-control @error('email') is-invalid @enderror" type="text" name="email" required value="{{old('email')}}">
+                            <input placeholder="Email" id="floatingEmail" class="shadow-none form-control @error('email') is-invalid @enderror" type="text" name="email" value="{{old('email')}}">
                             <label for="floatingEmail" class="font-futura-reztya">Email</label>
                             @error('email')
                             <div class="invalid-feedback">
-                                Email harap tidak kosong.
+                                Email wajib diisi
                             </div>
                             @enderror
                         </div>
                         <div class="form-floating mb-2">
-                            <input placeholder="Password" id="floatingPassword" class="shadow-none form-control @error('password') is-invalid @enderror" type="password" name="password" required value="{{old('password')}}">
+                            <input placeholder="Password" id="floatingPassword" class="shadow-none form-control @error('password') is-invalid @enderror" type="password" name="password" value="{{old('password')}}">
                             <label for="floatingPassword" class="font-futura-reztya">Kata Sandi</label>
                             @error('password')
                             <div class="invalid-feedback">
-                                Kata sandi harap tidak kosong.
+                                Kata sandi wajib diisi
                             </div>
                             @enderror
                         </div>
                         <div class="form-floating mb-2">
-                            <input placeholder="Confirm Password" id="floatingConfirmPassword" class="shadow-none form-control @error('confirm_password') is-invalid @enderror" type="password" name="confirm_password" required value="{{old('confirm_password')}}">
+                            <input placeholder="Confirm Password" id="floatingConfirmPassword" class="shadow-none form-control @error('confirm_password') is-invalid @enderror" type="password" name="confirm_password" value="{{old('confirm_password')}}">
                             <label for="floatingConfirmPassword" class="font-futura-reztya">Konfirmasi Kata Sandi</label>
                             @error('confirm_password')
                             <div class="invalid-feedback">
-                                Konfirmasi kata sandi harap tidak kosong / Konfirmasi kata sandi tidak sesuai dengan kata sandi.
+                                Konfirmasi kata sandi wajib diisi / Konfirmasi kata sandi tidak sesuai dengan kata sandi.
                             </div>
                             @enderror
                         </div>
