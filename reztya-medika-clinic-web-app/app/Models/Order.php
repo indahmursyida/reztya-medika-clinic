298cfc591ef95e2da3cbf46c9d94d62430ae5671
indahmursyida <indahbahrina@gmail.com>
2022-10-25 22:13:11 +0700

    Add Order, OrderCancel, OrderDetail Model and Migration

---
+++ reztya-medika-clinic-web-app/app/Models/Order.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'order_id';
+    protected $fillable = [
+        'order_detail_id', 
+        'cancel_id', 
+        // 'payment_receipt_id', 
+        'user_id', 
+        'order_date', 
+        'status'
+    ];
+    public function orderDetail(){
+        return $this->hasOne(OrderDetail::class, 'order_detail_id');
+    }
+
+    public function cancel(){
+        return $this->hasOne(OrderCancel::class, 'cancel_id');
+    }
+
+    public function user(){
+        return $this->hasOne(User::class, 'user_id');
+    }
+}
---
+++ reztya-medika-clinic-web-app/app/Models/OrderDetail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderDetail extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'order_detail_id';
+    protected $fillable = [
+        'service_id', 
+        'product_id',
+        'schedule_id',  
+        'quantity'
+    ];
+    public function service(){
+        return $this->hasMany(Service::class, 'service_id');
+    }
+
+    public function product(){
+        return $this->hasMany(Product::class, 'product_id');
+    }
+
+    public function schedule(){
+        return $this->hasMany(Service::class, 'schedule_id');
+    }
+
+    public function order(){
+        return $this->belongsTo(Order::class, 'order_id');
+    }
+}
