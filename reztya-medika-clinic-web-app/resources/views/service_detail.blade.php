b579d3001de678b5dd17fd541e69f5fd450f54b6
indahmursyida <indahbahrina@gmail.com>
2022-10-25 16:37:23 +0700

    Adjustment pilih jadwal

--- reztya-medika-clinic-web-app/resources/views/service_detail.blade.php
+++ reztya-medika-clinic-web-app/resources/views/service_detail.blade.php
@@ -7,7 +7,7 @@
 	<div class="py-3 text-center">
 		<h2 class="pb-3 font-alander-reztya unselectable">Detail Layanan Perawatan</h2>
 	</div>
-	<div class="row py-5">
+	<div class="row py-6">
 		<div class="col-sm-6 pe-5">
 			<img src="{{ asset('storage/' . $service->image_path) }}" class="img-fluid img-thumbnail">
 		</div>
@@ -17,15 +17,18 @@
 			<h5>Rp.{{ number_format($service->price, 0, '', '.') }}</h5>
 			<div class="my-5">
 				<p>{{ $service->description }}</p>
-			</DIv>
-			<label for="stock" class="my-2">Jumlah</label>
-			<input type="number" class="form-control form-quantity" id="quantity" name="quantity" min="1" max="{{ old('quantity') }}" value="{{ old('quantity', 1) }}">
-			<div class="col-md-8">
+			</div>
+			<label for="schedule_id" class="my-2">Pilih jadwal: </label>
+			<div>
 				<select class="form-select @error('schedule_id') is-invalid @enderror" id="schedule_id" name="schedule_id">
 					@foreach($schedules as $schedule) @if(old('schedule_id') == $schedule->schedule_id)
-					<option value="{{ $schedule->schedule_id }}" selected>{{ $schedule->start_time }} - {{ $schedule->end_time }}</option>
+					<option value="{{ $schedule->schedule_id }}" selected>
+					{{date('l, d F Y H:i', strtotime( $schedule->start_time ))}}
+					</option>
 					@else
-					<option value="{{ $schedule->schedule_id }}">{{ $schedule->start_time }} - {{ $schedule->end_time }}</option>
+					<option value="{{ $schedule->schedule_id }}">
+						{{date('l, d F Y H:i', strtotime( $schedule->start_time ))}}
+					</option>
 					@endif @endforeach
 				</select>
 				@error('schedule_id')
@@ -36,7 +39,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="d-flex justify-content-center pb-5">
+	<div class="d-flex justify-content-center pb-5 my-5">
 		<button class="btn btn-success"><i class="fa-solid fa-cart-shopping"></i> Tambahkan ke keranjang</button>
 	</div>
 
