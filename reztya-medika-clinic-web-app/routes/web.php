427ee757de6366f32b5207b8db9c563af851d7a2
Xenon468 <59041286+Xenon468@users.noreply.github.com>
2022-11-12 14:13:26 +0700

    Routes

--- reztya-medika-clinic-web-app/routes/web.php
+++ reztya-medika-clinic-web-app/routes/web.php
@@ -56,21 +56,8 @@ Route::post('/edit-profile/{username}', [ProfileController::class, 'editProfile'
 // Change Password
 Route::post('/change-password/{username}', [ProfileController::class, 'changePassword'])->middleware('auth');
 
-//Product
+// Products
 Route::get('/view-products', [ProductController::class, 'view']);
-Route::get('/manage-products', [ProductController::class, 'index']);
-Route::get('/product-detail/{id}', [ProductController::class, 'show']);
-Route::post('/delete-product/{id}', [ProductController::class, 'destroy']);
-Route::get('/add-product', [ProductController::class, 'create']);
-Route::post('/store-product', [ProductController::class, 'store']);
-Route::get('/edit-product/{id}', [ProductController::class, 'edit']);
-Route::put('/update-product/{id}', [ProductController::class, 'update']);
-Route::get('/view-products/search-product', [ProductController::class, 'search']);
-Route::get('/view-products/filter/name/a-to-z', [ProductController::class, 'filterAtoZ']);
-Route::get('/view-products/filter/name/z-to-a', [ProductController::class, 'filterZtoA']);
-Route::get('/view-products/filter/price/high-to-low', [ProductController::class, 'filterPriceHightoLow']);
-Route::get('/view-products/filter/price/low-to-high', [ProductController::class, 'filterPriceLowtoHigh']);
-Route::get('/view-products/filter/category/{category_name}', [ProductController::class, 'filterCategory']);
 Route::get('/manage-products', [ProductController::class, 'index'])->middleware('admin');
 Route::get('/product-detail/{id}', [ProductController::class, 'show'])->middleware('admin');
 Route::post('/delete-product/{id}', [ProductController::class, 'destroy'])->middleware('admin');
@@ -78,8 +65,16 @@ Route::get('/add-product', [ProductController::class, 'create'])->middleware('ad
 Route::post('/store-product', [ProductController::class, 'store'])->middleware('admin');
 Route::get('/edit-product/{id}', [ProductController::class, 'edit'])->middleware('admin');
 Route::put('/update-product/{id}', [ProductController::class, 'update'])->middleware('admin');
+Route::get('/view-products/search-product', [ProductController::class, 'search']);
+Route::get('/view-products/filter/name/a-to-z', [ProductController::class, 'filterAtoZ']);
+Route::get('/view-products/filter/name/z-to-a', [ProductController::class, 'filterZtoA']);
+Route::get('/view-products/filter/price/high-to-low', [ProductController::class, 'filterPriceHightoLow']);
+Route::get('/view-products/filter/price/low-to-high', [ProductController::class, 'filterPriceLowtoHigh']);
+Route::get('/view-products/filter/category/{category_name}', [ProductController::class, 'filterCategory']);
 
-//Service
+
+// Services
+Route::get('/view-services', [ServiceController::class, 'view']);
 Route::get('/manage-services', [ServiceController::class, 'index'])->middleware('admin');
 Route::get('/service-detail/{id}', [ServiceController::class, 'show'])->middleware('admin');
 Route::post('/delete-service/{id}', [ServiceController::class, 'destroy'])->middleware('admin');
@@ -87,7 +82,14 @@ Route::get('/add-service', [ServiceController::class, 'create'])->middleware('ad
 Route::post('/store-service', [ServiceController::class, 'store'])->middleware('admin');
 Route::get('/edit-service/{id}', [ServiceController::class, 'edit'])->middleware('admin');
 Route::put('/update-service/{id}', [ServiceController::class, 'update'])->middleware('admin');
+Route::get('/view-services/search-services', [ServiceController::class, 'search']);
+Route::get('/view-services/filter/name/a-to-z', [ServiceController::class, 'filterAtoZ']);
+Route::get('/view-services/filter/name/z-to-a', [ServiceController::class, 'filterZtoA']);
+Route::get('/view-services/filter/price/high-to-low', [ServiceController::class, 'filterPriceHightoLow']);
+Route::get('/view-services/filter/price/low-to-high', [ServiceController::class, 'filterPriceLowtoHigh']);
+Route::get('/view-services/filter/category/{category_name}', [ServiceController::class, 'filterCategory']);
 
+// Schedules
 Route::get('/manage-schedules', [ScheduleController::class, 'index']);
 Route::get('/add-schedule', [ScheduleController::class, 'add']);
 Route::post('/add-schedule', [ScheduleController::class, 'store']);
@@ -104,24 +106,11 @@ Route::get('/view-users', [BanController::class, 'viewUsers'])->middleware('admi
 Route::post('/ban-user/{username}', [BanController::class, 'banUser'])->middleware('admin');
 Route::post('/unban-user/{username}', [BanController::class, 'unbanUser'])->middleware('admin');
 
-//Category
+// Category
 Route::get('/manage-categories', [CategoryController::class, 'index']);
 Route::post('/delete-category/{id}', [CategoryController::class, 'destroy']);
 Route::get('/add-category', [CategoryController::class, 'create']);
 Route::post('/store-category', [CategoryController::class, 'store']);
 Route::get('/edit-category/{id}', [CategoryController::class, 'edit']);
 Route::put('/update-category/{id}', [CategoryController::class, 'update']);
-Route::get('/view-services', [ServiceController::class, 'view']);
-Route::get('/manage-services', [ServiceController::class, 'index']);
-Route::get('/service-detail/{id}', [ServiceController::class, 'show']);
-Route::post('/delete-service/{id}', [ServiceController::class, 'destroy']);
-Route::get('/add-service', [ServiceController::class, 'create']);
-Route::post('/store-service', [ServiceController::class, 'store']);
-Route::get('/edit-service/{id}', [ServiceController::class, 'edit']);
-Route::put('/update-service/{id}', [ServiceController::class, 'update']);
-Route::get('/view-services/search-services', [ServiceController::class, 'search']);
-Route::get('/view-services/filter/name/a-to-z', [ServiceController::class, 'filterAtoZ']);
-Route::get('/view-services/filter/name/z-to-a', [ServiceController::class, 'filterZtoA']);
-Route::get('/view-services/filter/price/high-to-low', [ServiceController::class, 'filterPriceHightoLow']);
-Route::get('/view-services/filter/price/low-to-high', [ServiceController::class, 'filterPriceLowtoHigh']);
-Route::get('/view-services/filter/category/{category_name}', [ServiceController::class, 'filterCategory']);
+
