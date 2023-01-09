0d0f8791a58f46d2246160c0ab657eefb4ee1767
Xenon468 <59041286+Xenon468@users.noreply.github.com>
2023-01-09 08:49:46 +0700

    Delete additional carousel

--- reztya-medika-clinic-web-app/resources/views/home/home_page.blade.php
+++ reztya-medika-clinic-web-app/resources/views/home/home_page.blade.php
@@ -51,39 +51,6 @@
                 </div>
             </div>
         </div>
-            <div id="carouselExampleIndicators" class="carousel carousel-reztya slide card border-reztya border border-4" style="height: 400px !important;" data-bs-ride="true">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <a href="/home">
-                            <img src="{{url('storage/banner1.jpg')}}" class="d-block w-100" alt="Klinik Reztya Medika">
-                        </a>
-                    </div>
-                    <div class="carousel-item">
-                        <a class="d-flex justify-content-center" href="#">
-                            <img src="{{url('storage/banner2.jpg')}}" class="d-block w-100" alt="Klinik Reztya Medika">
-                        </a>
-                    </div>
-                    <div class="carousel-item">
-                        <a class="d-flex justify-content-center" href="#">
-                            <img src="{{url('storage/banner3.jpg')}}" class="d-block w-100" alt="Klinik Reztya Medika">
-                        </a>
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-        </div>
         <div class="row mt-5">
             <div class="d-flex justify-content-center">
                 <p class="h5 fw-bold unselectable font-alander-reztya">Best Sellers</p>
